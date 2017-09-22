8aaf5ae1d2ae88b99f6163ff0b9b00f2709b348d
Bo <bo@bos-macbook-pro.lan>
2017-09-22 09:33:19 -0700

    first commit

---
+++ a1/test.py
@@ -0,0 +1,15 @@
+
+import numpy as np
+import random
+
+def getRandomContext(C):
+      tokens = ["a", "b", "c", "d", "e"]
+      return tokens[random.randint(0,4)], \
+          [tokens[random.randint(0,4)] for i in xrange(2*C)]
+
+if __name__ == "__main__":
+    # print getRandomContext(5)
+    indices = [1]
+    indices.extend([2,3,4,5,6])
+    for i in xrange(1,len(indices) ):
+        print i
